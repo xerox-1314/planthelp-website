76ab1fe9d966d70c4f0f59b3977352c6fdb11067
danil013 <jilikov_dd@gkl-kemerovo.ru>
2023-02-21 20:40:30 +0700

    Add button and telegram bot info

--- app.py
+++ app.py
@@ -2,10 +2,17 @@ from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
+
 @app.route('/')
 @app.route('/home')
 def welcome():
     return render_template('main_window.html')
+
+
+@app.route('/define')
+def define():
+    return render_template('define_window.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True, port=8080)
