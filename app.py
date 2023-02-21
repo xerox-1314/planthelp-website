6ac1a94d7a34f34edb21d1b76d634277f9c43fc8
danial <dzilikov@gmail.com>
2023-02-21 18:34:04 +0700

    Add menu and main image on main window

---
+++ app.py
@@ -0,0 +1,11 @@
+from flask import Flask, render_template, url_for
+
+app = Flask(__name__)
+
+@app.route('/')
+@app.route('/home')
+def welcome():
+    return render_template('main_window.html')
+
+if __name__ == '__main__':
+    app.run(debug=True, port=8080)
